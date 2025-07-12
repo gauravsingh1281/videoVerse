55701241f6b8df945d558b68f60b2bd16b2f952c
Gaurav Pratap Singh <gs492979@gmail.com>
2025-07-13 01:10:56 +0530

    user logout controller created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -109,4 +109,27 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new apiResponse(200, {}, "User logged Out successfully"));
+});
+
+export { registerUser, loginUser, logoutUser };
