9effdf23257aea5511b13da12a63e538fdb74b0c
Gaurav Pratap Singh <gs492979@gmail.com>
2025-07-13 00:58:49 +0530

    auth middleware created.

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import { apiError } from "../utils/apiError";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyJwt = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      res.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer", "").trim();
+    if (!token) throw new apiError(401, "Unauthorized request");
+    const decodedTokenData = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const foundUser = await User.findById(decodedTokenData?._id).select(
+      "-password -refreshToken"
+    );
+    if (!foundUser) throw apiError(401, "Invalid Access Token");
+    req.user = foundUser;
+    next();
+  } catch (error) {
+    throw new Error(401, error?.message || "Invalid access token");
+  }
+});
